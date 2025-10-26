765df67a7c6c0ca95e89261667d7e83e8153249d
italo <italo.ab.leite@gmail.com>
2025-10-26 15:49:42 -0300

    feat: saida dados csharp

---
+++ LogicaProgramacao/SaidaDadosCSharp/SaidaDados.cs
@@ -0,0 +1,11 @@
+﻿namespace POO.LogicaProgramacao.SaidaDadosCSharp
+{
+    public class SaidaDados
+    {
+        static void Main (string[] args)
+        {
+            Console.WriteLine("Hello World");
+            Console.Write("Hello World");
+        }
+    }
+}
